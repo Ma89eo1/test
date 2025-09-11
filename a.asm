a8b9f67ee8ebf21e33cdbdbae70c655864086220
Ma89eo1 <140007089+Ma89eo1@users.noreply.github.com>
2025-09-11 19:32:21 +0200

    Update a.asm

--- a.asm
+++ a.asm
@@ -1,246 +1,310 @@
-org 100h
-jmp start
+; Snake Game in 16-bit x86 Assembly for emu8086
+; Features: VGA graphics, keyboard control, random food, score tracking
+; Controls: Arrow keys (Up/Down/Left/Right), Esc to quit
 
-; === MESSAGES ===
-prompt db 'MiniFS> $'
-unknown db 13,10,'Unknown command.',13,10,'$'
-helpmsg db 13,10,'Commands: NEW, LIST, VIEW, DEL, HELP, EXIT',13,10,'$'
-entername db 13,10,'Enter filename: $'
-entercontent db 13,10,'Enter content: $'
-filenotfound db 13,10,'File not found.',13,10,'$'
-newline db 13,10,'$'
+.model small
+.stack 100h
+.data
+    ; Game constants
+    GRID_WIDTH  equ 20        ; Grid is 20x12 (320x200, 16x16 pixels/cell)
+    GRID_HEIGHT equ 12
+    SNAKE_MAX   equ 100       ; Max snake length
+    VIDEO_SEG   equ 0A000h    ; VGA video memory
+    PIXEL_SIZE  equ 16        ; Each cell is 16x16 pixels
 
-buffer db 32 dup('$')
-file_names db 10 dup(8 dup('$'))  ; 10 filenames, 8 chars each
-file_contents db 10 dup(32 dup('$')) ; corresponding content slots
+    ; Colors
+    COLOR_SNAKE equ 10        ; Green for snake
+    COLOR_FOOD  equ 12        ; Red for food
+    COLOR_BG    equ 0         ; Black background
 
-start:
+    ; Snake data
+    snake_x     dw SNAKE_MAX dup(0)  ; X coords (in cells)
+    snake_y     dw SNAKE_MAX dup(0)  ; Y coords (in cells)
+    snake_len   dw 3                 ; Initial length
+    snake_dir   db 3                 ; 0=up, 1=down, 2=left, 3=right
+    head_x      dw 10                ; Head start position
+    head_y      dw 6
+    food_x      dw 0                 ; Food position
+    food_y      dw 0
+    score       dw 0                 ; Score counter
+
+    ; Messages
+    msg_gameover db 'Game Over! Score: $'
+    msg_score    db 5 dup(0), '$'    ; Buffer for score digits
+
+.code
+main proc
     mov ax, @data
     mov ds, ax
 
-main_loop:
-    mov ah, 09h
-    mov dx, offset prompt
+    ; Initialize VGA mode 13h (320x200, 256 colors)
+    mov ax, 0013h
+    int 10h
+
+    ; Initialize snake at (10,6), moving right
+    mov snake_x[0], 10
+    mov snake_y[0], 6
+    mov snake_x[2], 9
+    mov snake_y[2], 6
+    mov snake_x[4], 8
+    mov snake_y[4], 6
+
+    ; Place first food
+    call spawn_food
+
+game_loop:
+    ; Clear keyboard buffer
+    mov ah, 0Ch
+    mov al, 0
     int 21h
 
-    lea si, buffer
-    call read_line
-    lea si, buffer
-    call to_upper
+    ; Check for keypress (non-blocking)
+    mov ah, 01h
+    int 16h
+    jz no_key
+    mov ah, 00h
+    int 16h
+    cmp ah, 01h         ; Esc key
+    je game_over
+    cmp ah, 48h         ; Up arrow
+    je set_up
+    cmp ah, 50h         ; Down arrow
+    je set_down
+    cmp ah, 4Bh         ; Left arrow
+    je set_left
+    cmp ah, 4Dh         ; Right arrow
+    je set_right
+    jmp no_key
 
-    lea si, buffer
-    call compare_new    je do_new
-    lea si, buffer
-    call compare_list   je do_list
-    lea si, buffer
-    call compare_view   je do_view
-    lea si, buffer
-    call compare_del    je do_del
-    lea si, buffer
-    call compare_help   je do_help
-    lea si, buffer
-    call compare_exit   je do_exit
+set_up:
+    cmp snake_dir, 1    ; Prevent reversing into self
+    je no_key
+    mov snake_dir, 0
+    jmp no_key
+set_down:
+    cmp snake_dir, 0
+    je no_key
+    mov snake_dir, 1
+    jmp no_key
+set_left:
+    cmp snake_dir, 3
+    je no_key
+    mov snake_dir, 2
+    jmp no_key
+set_right:
+    cmp snake_dir, 2
+    je no_key
+    mov snake_dir, 3
 
-    mov dx, offset unknown
-    mov ah, 09h
-    int 21h
-    jmp main_loop
+no_key:
+    ; Update snake position
+    mov bx, snake_len
+    shl bx, 1           ; Index for word array
+    dec bx
+    dec bx              ; Start from tail
+tail_loop:
+    cmp bx, 0
+    jle move_head
+    mov ax, snake_x[bx-2]
+    mov snake_x[bx], ax
+    mov ax, snake_y[bx-2]
+    mov snake_y[bx], ax
+    sub bx, 2
+    jmp tail_loop
 
-; === COMMANDS ===
-
-do_new:
-    mov dx, offset entername
-    mov ah, 09h
-    int 21h
-
-    lea si, buffer
-    call read_line
-
-    ; find first empty slot
-    mov di, offset file_names
-    mov cx, 10
-.findslot:
-    mov al, [di]
-    cmp al, '$'
-    je .slot_found
-    add di, 8
-    loop .findslot
-    jmp main_loop
-.slot_found:
-    ; copy filename
-    lea si, buffer
-    mov bx, di
-    mov cx, 8
-.copy_loop:
-    mov al, [si]
-    mov [bx], al
-    inc si
+move_head:
+    mov ax, head_x
+    mov bx, head_y
+    cmp snake_dir, 0    ; Up
+    je move_up
+    cmp snake_dir, 1    ; Down
+    je move_down
+    cmp snake_dir, 2    ; Left
+    je move_left
+    ; Right
+    inc ax
+    jmp update_pos
+move_up:
+    dec bx
+    jmp update_pos
+move_down:
     inc bx
-    dec cx
-    jnz .copy_loop
-    jmp main_loop
+    jmp update_pos
+move_left:
+    dec ax
 
+update_pos:
+    mov head_x, ax
+    mov head_y, bx
+    mov snake_x[0], ax
+    mov snake_y[0], bx
 
-do_list:
-    mov di, offset file_names
-    mov cx, 10
-.list_loop:
-    mov dx, di
+    ; Check wall collision
+    cmp ax, GRID_WIDTH
+    jae game_over
+    cmp ax, 0
+    jl game_over
+    cmp bx, GRID_HEIGHT
+    jae game_over
+    cmp bx, 0
+    jl game_over
+
+    ; Check self collision
+    mov cx, snake_len
+    mov si, 2           ; Start from second segment
+self_coll_loop:
+    cmp si, cx
+    jge no_coll
+    mov dx, snake_x[si]
+    cmp ax, dx
+    jne next_seg
+    mov dx, snake_y[si]
+    cmp bx, dx
+    je game_over
+next_seg:
+    add si, 2
+    jmp self_coll_loop
+
+no_coll:
+    ; Check food collision
+    cmp ax, food_x
+    jne no_food
+    cmp bx, food_y
+    jne no_food
+    inc snake_len
+    inc score
+    call spawn_food
+
+no_food:
+    ; Clear screen (black)
+    mov ax, VIDEO_SEG
+    mov es, ax
+    xor di, di
+    mov cx, 320*200
+    mov al, COLOR_BG
+    rep stosb
+
+    ; Draw snake
+    mov cx, snake_len
+    xor si, si
+draw_snake:
+    mov ax, snake_x[si]
+    mov bx, snake_y[si]
+    call draw_cell
+    add si, 2
+    loop draw_snake
+
+    ; Draw food
+    mov ax, food_x
+    mov bx, food_y
+    mov dl, COLOR_FOOD
+    call draw_cell
+
+    ; Delay (adjust for speed)
+    mov cx, 0FFFFh
+delay_loop:
+    loop delay_loop
+
+    jmp game_loop
+
+game_over:
+    ; Switch to text mode
+    mov ax, 0003h
+    int 10h
+
+    ; Display game over and score
+    mov dx, offset msg_gameover
     mov ah, 09h
     int 21h
-    add di, 8
-    loop .list_loop
-    jmp main_loop
 
-
-do_view:
-    mov dx, offset entername
+    ; Convert score to string
+    mov ax, score
+    mov di, offset msg_score
+    call num_to_str
+    mov dx, offset msg_score
     mov ah, 09h
     int 21h
 
-    lea si, buffer
-    call read_line
+    ; Exit
+    mov ax, 4C00h
+    int 21h
 
-    mov di, offset file_names
-    mov cx, 10
-.search_loop:
+main endp
+
+; Draw 16x16 cell at (AX,BX) with color DL
+draw_cell proc
+    push ax
+    push bx
     push cx
-    lea bx, buffer
-    lea si, di
-    mov cx, 8
-    call compare_string
-    cmp ax,1
-    je .found
-    add di, 8
+    push di
+    shl ax, 4        ; AX *= 16 (pixel x)
+    shl bx, 4        ; BX *= 16 (pixel y)
+    mov di, bx
+    imul di, 320     ; DI = y*320
+    add di, ax       ; DI += x
+    mov ax, VIDEO_SEG
+    mov es, ax
+    mov cx, PIXEL_SIZE
+draw_row:
+    push cx
+    mov cx, PIXEL_SIZE
+    mov al, dl
+    rep stosb        ; Draw row of pixels
+    add di, 320-PIXEL_SIZE
     pop cx
-    loop .search_loop
-    mov dx, offset filenotfound
-    mov ah, 09h
-    int 21h
-    jmp main_loop
-.found:
-    ; display content
-    mov dx, di
-    mov ah, 09h
-    int 21h
-    jmp main_loop
-
-
-do_del:
-    mov dx, offset entername
-    mov ah, 09h
-    int 21h
-
-    lea si, buffer
-    call read_line
-    ; simple deletion: overwrite with '$'
-    mov di, offset file_names
-    mov cx, 10
-.del_loop:
-    lea bx, di
-    lea si, buffer
-    mov cx, 8
-    call compare_string
-    cmp ax,1
-    je .del_found
-    add di, 8
-    loop .del_loop
-    jmp main_loop
-.del_found:
-    mov cx, 8
-.fill_loop:
-    mov [di], '$'
-    inc di
-    dec cx
-    jnz .fill_loop
-    jmp main_loop
-
-
-do_help:
-    mov dx, offset helpmsg
-    mov ah, 09h
-    int 21h
-    jmp main_loop
-
-
-do_exit:
+    loop draw_row
+    pop di
+    pop cx
+    pop bx
+    pop ax
     ret
+draw_cell endp
 
-; === UTILITIES ===
+; Spawn food at random position
+spawn_food proc
+    ; Simple PRNG using timer tick
+    mov ah, 00h
+    int 1Ah          ; Get timer ticks in DX
+    mov ax, dx
+    xor dx, dx
+    mov cx, GRID_WIDTH
+    div cx           ; AX % GRID_WIDTH
+    mov food_x, dx
+    mov ax, dx
+    xor dx, dx
+    mov cx, GRID_HEIGHT
+    div cx
+    mov food_y, dx
+    ret
+spawn_food endp
 
-read_line:
-    mov cx,0
-.rl_loop:
-    mov ah,01h
-    int 21h
-    cmp al,13
-    je .rl_done
-    mov [si], al
-    inc si
+; Convert AX to decimal string at DI
+num_to_str proc
+    push ax
+    push bx
+    push cx
+    push dx
+    mov bx, 10
+    mov cx, 0
+num_loop:
+    xor dx, dx
+    div bx
+    add dl, '0'
+    push dx
     inc cx
-    cmp cx,31
-    je .rl_done
-    jmp .rl_loop
-.rl_done:
-    mov [si],'$'
-    ret
-
-
-to_upper:
-    mov cx,32
-.upper_loop:
-    mov al,[si]
-    cmp al,'a'
-    jb .next_upper
-    cmp al,'z'
-    ja .next_upper
-    sub al,32
-    mov [si],al
-.next_upper:
-    inc si
-    loop .upper_loop
-    ret
-
-compare_new:  mov di,offset cmd_new  call compare  ret
-compare_list: mov di,offset cmd_list call compare  ret
-compare_view: mov di,offset cmd_view call compare  ret
-compare_del:  mov di,offset cmd_del  call compare  ret
-compare_help: mov di,offset cmd_help call compare  ret
-compare_exit: mov di,offset cmd_exit call compare  ret
-
-compare:
-.loop:
-    mov al,[si]
-    cmp al,0
-    je .match
-    cmp al,[di]
-    jne .fail
-    inc si
+    cmp ax, 0
+    jne num_loop
+store_loop:
+    pop dx
+    mov [di], dl
     inc di
-    jmp .loop
-.match: mov ax,1 ret
-.fail:  mov ax,0 ret
-
-compare_string:
-    push cx dx
-.cmp_loop:
-    mov al,[si]
-    cmp al,[bx]
-    jne .no_match
-    inc si
-    inc bx
-    dec cx
-    jnz .cmp_loop
-    mov ax,1
-    jmp .done_cmp
-.no_match: mov ax,0
-.done_cmp:
-    pop dx cx
+    loop store_loop
+    mov byte ptr [di], '$'
+    pop dx
+    pop cx
+    pop bx
+    pop ax
     ret
+num_to_str endp
 
-cmd_new db 'NEW',0
-cmd_list db 'LIST',0
-cmd_view db 'VIEW',0
-cmd_del db 'DEL',0
-cmd_help db 'HELP',0
-cmd_exit db 'EXIT',0
+end main
